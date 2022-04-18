22a4945d528c85d799c6151e7517e566ad0ddcf2
SeJin Ahn <87055116+sejin423@users.noreply.github.com>
2022-04-18 21:27:39 +0900

    Update __init__.py

--- DDOKDDOK/__init__.py
+++ DDOKDDOK/__init__.py
@@ -29,7 +29,7 @@ def create_app():
         migrate.init_app(app, db)
     from DDOKDDOK import models
 
-    # 블루프린트
+    # blueprint
     from DDOKDDOK.views import main_views, auth_views, test_room_views
     app.register_blueprint(main_views.bp)
     app.register_blueprint(auth_views.bp)
